74486d27ed75db0fd801fccc0e9dd3cd1b0af390
GreenDude <schipor@gmail.com>
2011-06-29 23:09:44 +0300

    Spawning overlords

---
+++ js/behave.js
@@ -0,0 +1,17 @@
+/*jslint white:false */
+/*global document, window, setTimeout, clearTimeout, console, log, require, define */
+(function ($) {
+	
+	var MIEZ = {
+		
+		onReady: function () {
+			
+		
+			
+		}
+		
+	};
+	
+	$(document).ready(MIEZ.onReady);
+	
+}(window.jQuery));
