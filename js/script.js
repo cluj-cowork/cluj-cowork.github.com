9da880dea457f32ce1ca569bf550743f21d0912f
Stas SUȘCOV <stas@net.utcluj.ro>
2012-02-17 22:10:47 +0200

    Fixed twitter stream.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-/* Author:
-
-*/
-
-
-
-
-
+/**
+ * Live Twitter Stream
+ */
+$(function(){
+  $('#twitter-stream').liveTwitter(
+    'clujcowork', {
+      mode: 'user_timeline',
+      limit: 4,
+      refresh: true
+    }, function(){
+      $('#twitter-stream .loading').remove();
+    });
+});
