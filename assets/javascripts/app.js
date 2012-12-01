b1b4c86be289d0e8151fbf32a25887aa26ae47ad
Stas SUȘCOV <stas@net.utcluj.ro>
2012-12-01 05:49:27 +0200

    Added grid rotator.

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -2,7 +2,7 @@
   'use strict';
 
   var $doc = $(document);
-  var ClujCowork = {
+  window.ClujCowork = {
     init: function() {
       this.resizeStripes();
       $( window ).resize( this.resizeStripes );
@@ -13,6 +13,34 @@
       var width = document.documentElement.offsetWidth - 1000;
       var $stripes = $( '.stripe' );
       $stripes.css( 'margin-right', '-' + width / 2 + 'px' );
+    },
+    // Enable homepage grid rotator
+    enableImagesGrid: function( selector ) {
+      $( selector ).gridrotator( {
+        rows : 4,
+        columns : 15,
+        animType : 'random',
+        w1024 : {
+          rows : 5,
+          columns : 6
+        },
+        w768 : {
+          rows : 5,
+          columns : 5
+        },
+        w480 : {
+          rows : 6,
+          columns : 4
+        },
+        w320 : {
+          rows : 7,
+          columns : 4
+        },
+        w240 : {
+          rows : 7,
+          columns : 3
+        },
+      } );
     }
   };
 
